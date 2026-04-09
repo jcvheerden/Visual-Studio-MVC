9c9ef70058003cb938e7085b654624863c360960
jcvheerden <ST10316473@vcconnect.edu.za>
2026-04-09 20:05:59 +0200

    Fixed

--- MVC/prjHealthCareSystem/obj/Debug/net10.0/prjHealthCareSystem.AssemblyInfo.cs
+++ MVC/prjHealthCareSystem/obj/Debug/net10.0/prjHealthCareSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("prjHealthCareSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53520fe2e77f7bb6bd465bd2dde4f45edf833221")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d38cf8da819e16a409e5a0661c19d88740a97d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("prjHealthCareSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prjHealthCareSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
